01bd9eeb3eed34abae35f5617395885458990cf9
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-06-09 00:21:01 -0400

    Add container to run a web server

--- virtualisation.nix
+++ virtualisation.nix
@@ -7,27 +7,28 @@
      dockerCompat = false;
   };
  };
- # container testing
- # containers = {
- #   web1 = {
- #     ephemeral = true;
- #     autoStart = true;
- #     bindMounts = {
- #       "/var/www/foo" = {
- #       hostPath = "/var/www/foo";
- #       isReadOnly = true;
- #       };
- #     };
- #     config = { config, pkgs, ... }: {
- #       services.httpd.enable = true;
- #       services.httpd.adminAddr = "foo@example.org";
- #       services.httpd.virtualHosts = {
- #         "rpi4.local" = {
- #          documentRoot = "/var/www/foo";
- #          http2 = true;
- #         };
- #       };
- #     };
- #   };
- # };
+  # container testing
+  containers = {
+    webshit = {
+      ephemeral = true;
+      autoStart = true;
+      bindMounts = {
+        "/var/www/" = {
+        hostPath = "/var/www/";
+        isReadOnly = true;
+        };
+      };
+      config = { config, pkgs, ... }: {
+        services.httpd.enable = true;
+        services.httpd.adminAddr = "contact@rpi4.local";
+        services.httpd.virtualHosts = {
+          "rpi4.local" = {
+           hostName = "rpi4.local";
+           documentRoot = "/var/www/";
+           http2 = true;
+          };
+        };
+      };
+    };
+  };
 }
