c1b5783a06b98a62332f9ec6301466475214c06e
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-06-11 21:51:14 -0400

    Add SSH public key

--- users.nix
+++ users.nix
@@ -10,6 +10,7 @@
        openssh = { 
           authorizedKeys.keys = [
             "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACxewoWf/RGhUfbJ8DYk8fLmdW4FfSwTIkyFkjlYdxV24lqVOTopTQP0tHIbARd8WMKxKiLyo6Eb/YtAHnPYSJvlwADin9ahimF72BdQ6/3o+50F0k6GIY0WLZcyK6Z3JD39NqYeO7ZCC/IVXHfbCT9fAp8DvCBFntDnbp6McukWHe+EA== alc@Adrians-Mac-mini.local"
+            "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAH19WN6f0fyJUCR8vY5p0NTWOgWZkpMBYnwhUjZkAyM+F/u5LRCuMv7SawKBMOcf3+04aUleCUCadZDCn0ifzmJeQFvZaUJdlylyv4OK39JUiYH7FTCG5hcSU0zlYX8/1GYgkQQeo4x1HSGDh8Zr89FazEkRurUuvQQnz8SblSOZ5h4Bg== alc@ipp"
           ];
        };
     };
