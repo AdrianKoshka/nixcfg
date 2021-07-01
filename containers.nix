8b5c10a537a98743ee62b4bdc1f45d6539ea6f35
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-06-30 22:27:27 -0400

    Revert "Move statping to a podman pod setup"
    This reverts commit 9e53bf3df75ddb7ae2ed79d4cff07a063f983ef8.

--- containers.nix
+++ containers.nix
@@ -5,8 +5,21 @@
  virtualisation.oci-containers = {
    backend = "podman";
    containers = {
-    statping-server = import ./statping-server.nix;
-    statping-postgres = import ./statping-postgres.nix;
+    statping = {
+     image = "docker.io/statping/statping";
+     volumes = [
+      "/var/containers/statping/certs/cert.pem:/app/server.crt:z"
+      "/var/containers/statping/certs/key.pem:/app/server.key:z"
+      "/var/containers/statping/app/:/app/"
+     ];
+     ports = [
+      "443:443"
+     ];
+     environment = {
+      BASE_PATH = "statping";
+     };
+     autoStart = true;
+    };
    };
  };
 }
--- services.nix
+++ services.nix
@@ -1,18 +1,6 @@
 { config, pkgs, ...}:
 
 {
- systemd = {
-  services = {
-   create-statping-pod = with config.virtualisation.oci-containers; {
-    serviceConfig.Type = "oneshot";
-    wantedBy = [ "${backend}-statping-postgres.service" ];
-    script = ''
-     ${pkgs.podman}/bin/podman pod exists statping || \
-     ${pkgs.podman}/bin/podman pod create -n statping -p '443:443'
-    '';
-   };
-  };
- };
  services = {
   # List services that you want to enable:
   xserver = {
